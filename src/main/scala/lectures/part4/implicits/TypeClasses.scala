c9b2d8024e79b739116d255f02d4d9291c5f8d6d
Nalin28 <nalinmunshi@gmail.com>
2019-06-24 11:59:18 +0530

    type classes

--- src/main/scala/lectures/part4/implicits/TypeClasses.scala
+++ src/main/scala/lectures/part4/implicits/TypeClasses.scala
@@ -1,5 +1,103 @@
 package lectures.part4.implicits
 
-object TypeClasses {
+object TypeClasses extends App{
+// takes a type and sees what operations can be applied to that type
+trait HTMLWritable{
 
+    def toHtml: String
+  }
+
+  case class User(name: String, age: Int, email: String) extends HTMLWritable{
+    override def toHtml: String = s"<div>$name ($age yo) <a href=$email/></div>"
+  }
+
+  User("John", 22, "john.snow").toHtml
+
+  /*
+  1 - for the types WE write
+  2 - ONE implementation out of quite a number
+   */
+
+  // option 2 - pattern matching
+  object HTMLSerializerPM{
+    def serialization(value: Any) = value match{
+      case User(n, a, e) =>
+      // case java.util.Date =>
+      case _ =>
+    }
+  }
+  /*
+  1 - lost type safety
+  2 - need to modify the code every time
+  3 - still ONE implementation
+   */
+
+  trait HTMLSerializer[T]{
+    def serialize(user: T): String
+  }
+  implicit object UserSerializer extends HTMLSerializer[User]{
+    override def serialize(user: User): String = s"<div>${user.name} (${user.age} yo) <a href=${user.email}/></div>"
+  }
+  val john = User("John", 22, "john.snow")
+  println(UserSerializer.serialize(john))
+
+  //1 - we can define serializers for other types
+  import java.util.Date
+  object DateSerializer extends HTMLSerializer[Date]{
+    override def serialize(date: Date): String = s"<div>${date.toString}</div>"
+
+  }
+  //2 - we can define MULTIPLE serializers
+  object PartialUserSerializer extends HTMLSerializer[User]{
+    override def serialize(user: User): String = s"<div>${user.name}</div>"
+  }
+
+  // TYPE CLASS
+  trait MyTypeClassTemplate[T]{
+    def action(value: T): String
+  }
+
+  /*
+  Equality
+   */
+
+  trait Equal[T]{
+    def equal(a: T, b: T): Boolean
+  }
+
+  implicit object NameEqual extends Equal[User]{
+    override def equal(a: User, b: User): Boolean = a.name == b.name
+  }
+
+  object NameAndEmailEqual extends Equal[User]{
+    override def equal(a: User, b: User): Boolean = a.name == b.name && a.email == b.email
+  }
+
+  // part 2
+  object HTMLSerializer{
+    def serialize[T](value: T)(implicit serializer: HTMLSerializer[T]): String =
+      serializer.serialize(value)
+  }
+
+  implicit object IntSerializer extends HTMLSerializer[Int]{
+    override def serialize(value: Int): String = s"<div style: color=blue>$value</div>"
+  }
+
+  println(HTMLSerializer.serialize(42))
+  println(HTMLSerializer.serialize(john))
+
+  /*
+  Exercise: Implement the TC pattern for the Equality tc.
+   */
+
+  object Equal{
+    def equal[T](a: T, b: T)(implicit equalizer: Equal[T]): Boolean = equalizer.equal(a, b)
+  }
+
+  val anotherJohn = User("John", 45, "yo.yo")
+
+  println(Equal(john, anotherJohn))
+
+  // AD-HOC polymorphism -
 }
+

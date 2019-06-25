85e0b74d8aa9d6930ef8bf401c17f350db9bf34b
Nalin28 <nalinmunshi@gmail.com>
2019-06-25 10:30:20 +0530

    equality entry

--- src/main/scala/exercises/EqualityPlayground.scala
+++ src/main/scala/exercises/EqualityPlayground.scala
@@ -1,5 +1,22 @@
 package exercises
 
+import lectures.part4.implicits.TypeClasses.User
+
 object EqualityPlayground {
 
+  /*
+  Equality
+   */
+
+  trait Equal[T]{
+    def equal(a: T, b: T): Boolean
+  }
+
+  implicit object NameEqual extends Equal[User]{
+    override def equal(a: User, b: User): Boolean = a.name == b.name
+  }
+
+  object NameAndEmailEqual extends Equal[User]{
+    override def equal(a: User, b: User): Boolean = a.name == b.name && a.email == b.email
+  }
 }

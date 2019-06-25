242549e4c49722e3a59fc48c2eec323e40405c88
Nalin28 <nalinmunshi@gmail.com>
2019-06-25 11:05:52 +0530

    type safety

--- src/main/scala/exercises/EqualityPlayground.scala
+++ src/main/scala/exercises/EqualityPlayground.scala
@@ -2,7 +2,7 @@ package exercises
 
 import lectures.part4.implicits.TypeClasses.{User, john}
 
-object EqualityPlayground {
+object EqualityPlayground extends App{
 
   /*
   Equality
@@ -32,4 +32,31 @@ object EqualityPlayground {
   val anotherJohn = User("John", 45, "yo.yo")
 
   println(Equal(john, anotherJohn))
+
+  /*
+  Exercise - improve the Equal TC with an implicit conversion class
+  ===(anotherValue: T)
+  !==(anotherValue: T)
+   */
+  implicit class TypeSafeEqual[T](value: T){
+   def ===(anotherValue: T)(implicit equalizer: Equal[T]): Boolean = equalizer.equal(value, anotherValue)
+    def !==(anotherValue: T)(implicit equalizer: Equal[T]): Boolean = !equalizer.equal(value, anotherValue)
+  }
+
+  println(john === anotherJohn)
+  println(john !== anotherJohn)
+/*
+  john.===(anotherJohn)
+  Is there a method === that takes  User type value (john)? => yes
+    new TypeSafeEqual[User](john).===(anotherJohn)
+  Is there an equalizer that implicitly is of type Equal[T]? yes
+    new TypeSafeEqual[User](john).===(anotherJohn)(NameEqual)
+ */
+  /*
+  TYPE SAFE
+   */
+  println(john == 43) // correct
+  //println(john === 43) // type safe since I cant even compile this
+
+
 }
